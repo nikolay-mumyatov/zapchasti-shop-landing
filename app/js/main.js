199a780bcfc740726c0e0be3ff8d34df19acbd4a
nikolay-mumyatov <nikolay.mumyatov@yandex.ru>
2020-02-25 21:47:23 +0300

    Плавный скролл к форме "Подробная заявка". Обновление файлов и структуры.

---
+++ app/js/main.js
@@ -0,0 +1,9 @@
+// Кнопка "Подробная заявка"
+
+$(function () {
+    $('#btn_get-price').click(function(e){
+        event.preventDefault();
+        scrollForm = $('.get_price-block').offset().top;
+        $('html, body').animate({scrollTop : scrollForm}, 1000);
+    });
+});
