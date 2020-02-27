cc7ac9e3acff5677eaeb92a0c42fc578fd8bc7f6
nikolay-mumyatov <nikolay.mumyatov@yandex.ru>
2020-02-27 23:02:51 +0300

    Обновление модальных окон.

--- app/js/main.js
+++ app/js/main.js
@@ -8,5 +8,37 @@ $(function () {
     });
 });
 
+
 // Модальное окно "Быстрая заявка"
 
+$(function () {
+    var modalBtn = $('.active-btn'),
+        modalWindow = $('.modal-bid'),
+        close = $('.close-btn');
+
+    modalBtn.on('click', function(){
+        modalWindow.toggleClass('modal-bid_active');
+    });
+
+    close.on('click', function(){
+        modalWindow.toggleClass('modal-bid_active');
+    });
+});
+
+
+// Модальное окно "Заказать обратный звонок"
+
+$(function () {
+    var modalBtn = $('.modal-phone-btn'),
+        modalWindow = $('.modal-phone'),
+        close = $('.close-btn-phone');
+
+    modalBtn.on('click', function(){
+        event.preventDefault();
+        modalWindow.toggleClass('modal-phone_active');
+    });
+
+    close.on('click', function(){
+        modalWindow.toggleClass('modal-phone_active');
+    });
+});
