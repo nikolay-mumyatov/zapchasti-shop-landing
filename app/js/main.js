9e65d43cf2bbd3fec1bacfd7416097689eb7f568
nikolay-mumyatov <nikolay.mumyatov@yandex.ru>
2020-02-28 21:38:15 +0300

    Окончательная сборка.

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,14 @@
 // Кнопка "Подробная заявка"
 
 $(function () {
-    $('#btn_get-price').click(function(e){
+    $('#btn_get-price').click(function (e) {
         event.preventDefault();
         scrollForm = $('.get_price-block').offset().top;
-        $('html, body').animate({scrollTop : scrollForm}, 1000);
+        $('html, body').animate({
+            scrollTop: scrollForm
+        }, 1000);
+        console.log(scrollForm);
+
     });
 });
 
@@ -16,11 +20,11 @@ $(function () {
         modalWindow = $('.modal-bid'),
         close = $('.close-btn');
 
-    modalBtn.on('click', function(){
+    modalBtn.on('click', function () {
         modalWindow.toggleClass('modal-bid_active');
     });
 
-    close.on('click', function(){
+    close.on('click', function () {
         modalWindow.toggleClass('modal-bid_active');
     });
 });
@@ -33,12 +37,35 @@ $(function () {
         modalWindow = $('.modal-phone'),
         close = $('.close-btn-phone');
 
-    modalBtn.on('click', function(){
+    modalBtn.on('click', function () {
         event.preventDefault();
         modalWindow.toggleClass('modal-phone_active');
     });
 
-    close.on('click', function(){
+    close.on('click', function () {
         modalWindow.toggleClass('modal-phone_active');
+    });
+});
+
+// Кнопка наверх
+
+$(function () {
+    var upBtn = $('.up-arrow'),
+        upTop = $('header').offset().top;
+
+    $(window).scroll(function (event) {
+        var top = $(window).scrollTop();
+
+        if (top >= 1362) {
+            upBtn.css('visibility', 'visible');
+        } else {
+            upBtn.css('visibility', 'hidden');
+        }
+    });
+
+    upBtn.on('click', function () {
+        $('html, body').animate({
+            scrollTop: upTop
+        }, 700);
     });
 });
